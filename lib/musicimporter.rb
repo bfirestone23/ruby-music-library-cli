2b7661a7dfdfa8344aa2e089fc95c67d3841931e
Brian Firestone <bfirestone2339@gmail.com>
2021-01-02 16:19:17 -0500

    commit tree e4e8bca1142f181f7ba7401204f5d85575d2ab8f

---
+++ lib/musicimporter.rb
@@ -0,0 +1,16 @@
+class MusicImporter
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path 
+    end
+
+    def files
+        @files ||= Dir.glob("#{path}/*.mp3").collect{|x| x.gsub("#{path}/", "")}
+    end
+
+    def import
+        files.each { |x| Song.create_from_filename(x) }
+    end
+
+end
